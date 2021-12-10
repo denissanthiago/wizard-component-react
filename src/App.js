ee1030e6a7b8adde5509469b5738091c090bbccd
Denis Pablo <dpablo@soaint.com>
2021-12-09 21:18:27 -0500

    Add Wizard component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import Wizard, {
+  WizardPages,
+  WizardButtonPrev,
+  WizardButtonNext,
+  useWizardProgress,
+} from "./Wizard";
 
-function App() {
+const Page1 = () => (
+  <div>
+    <h1>Pagina 1</h1>
+  </div>
+);
+
+const Page2 = () => (
+  <div>
+    <h1>Pagina 2</h1>
+  </div>
+);
+const Page3 = () => (
+  <div>
+    <h1>Pagina 3</h1>
+  </div>
+);
+
+const Progress = () => {
+  const { currentIndex, steps } = useWizardProgress();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wizard__progress">
+      State {currentIndex} of {steps}
     </div>
   );
-}
+};
+
+const Navigation = () => {
+  // const { nextPage, prevPage } = useWizardButtons();
+  return (
+    <div className="wizard__buttons">
+      <WizardButtonPrev className="wizard__buttons-left">
+        Anterior
+      </WizardButtonPrev>
+      <WizardButtonNext className="wizard__buttons-right">
+        Siguiente
+      </WizardButtonNext>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Wizard className="wizard">
+      <Progress />
+      <WizardPages className="wizard__content">
+        <Page1 />
+        <Page2 />
+        <Page3 />
+      </WizardPages>
+      <Navigation />
+    </Wizard>
+  );
+};
 
 export default App;
